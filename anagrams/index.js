7aed7b7f2c88c1735e451cc56bcfe359928c17a1
github username <github email address>
2020-01-30 10:26:11 -0600

    solved

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,28 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const aStrMap = buildMap(stringA);
+    const bStrMap = buildMap(stringB);
+
+    if (Object.keys(aStrMap).length !== Object.keys(bStrMap).length){
+        return false;
+    }
+
+    for (let char in aStrMap){
+        if (aStrMap[char] !== bStrMap[char]){
+            return false;
+        }
+    }
+    return true;
+}
+
+function buildMap(str){
+    const charMap = {};
+    for (let char of str.replace(/[^\w]/g, '').toLowerCase()){
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap;
+}
 
 module.exports = anagrams;
