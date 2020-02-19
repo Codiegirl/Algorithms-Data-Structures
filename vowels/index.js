20e0d977d1c50caa3ea2cfb64dc05141c6db0353
github username <github email address>
2020-02-19 09:35:30 -0600

    solved

--- vowels/index.js
+++ vowels/index.js
@@ -8,16 +8,22 @@
 //   vowels('Why?') --> 0
 
 function vowels(str) {
-    let counter = 0;
-    const letters = ['a', 'e', 'i', 'o', 'u'];
-
-    for (let elem of str.toLowerCase()){
-        if (letters.includes(elem)){
-            counter ++
-        }
-    }
-
-    return counter
+   const found = str.match(/[aeiou]/gi)//g makes sure you don't stop. i is case sensitive
+   //if found is an array return found.length. if null return 
+   return found ? found.length : 0;
 }
 
 module.exports = vowels;
+
+
+
+// let counter = 0;
+//     const letters = ['a', 'e', 'i', 'o', 'u'];
+
+//     for (let elem of str.toLowerCase()){
+//         if (letters.includes(elem)){
+//             counter ++
+//         }
+//     }
+
+//     return counter
